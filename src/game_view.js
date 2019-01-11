03d92099c35b629afaa7a00ba4c829cf102ef3e6
gwang <guanw88@gmail.com>
2019-01-11 11:44:11 -0800

    Remove keybind when new tile is generated

--- src/game_view.js
+++ src/game_view.js
@@ -10,38 +10,52 @@ GameView.MOVES = {
 };
 
 GameView.prototype.bindKeyHandlers = function bindKeyHandlers(tile) {
+    render = this.render.bind(this);
 
-    document.addEventListener('keypress', (event) => {
+    function handleKeypress(event) {
         if (event.keyCode == 97) {
             tile.shift(-1); // a
-            requestAnimationFrame(this.render.bind(this));
+            requestAnimationFrame(render);
         } else if (event.keyCode == 100) {
             tile.shift(1); // d
-            requestAnimationFrame(this.render.bind(this));
+            requestAnimationFrame(render);
         } else if (event.keyCode == 119) {
             tile.toggleLetter();
-            requestAnimationFrame(this.render.bind(this));
+            requestAnimationFrame(render);
         } else {
             // console.log("other key pressed ", event.keyCode);
         }
-    })
+    }
 
-    document.addEventListener('keydown', (event) => {
+    function handleKeydown(event) {
         if (event.keyCode == 37) {
-          tile.shift(-1); // left arrow
-          requestAnimationFrame(this.render.bind(this));
+            tile.shift(-1); // left arrow
+            requestAnimationFrame(render);
         } else if (event.keyCode == 39) {
-          tile.shift(1); // right arrow
-          requestAnimationFrame(this.render.bind(this));
+            tile.shift(1); // right arrow
+            requestAnimationFrame(render);
         } else if (event.keyCode == 38) {
-          tile.toggleLetter();
-          requestAnimationFrame(this.render.bind(this));
+            tile.toggleLetter();
+            requestAnimationFrame(render);
         } else {
-        //   console.log("other key pressed 2 ", event.keyCode);
+            //   console.log("other key pressed 2 ", event.keyCode);
         }
-    })
+    }
+
+    document.handleKeypress = handleKeypress.bind(tile);
+    document.handleKeydown = handleKeydown.bind(tile);
+
+    document.addEventListener('keypress', document.handleKeypress);
+    document.addEventListener('keydown', document.handleKeydown);
 
 };
+
+GameView.prototype.unbindKeyHandlers = function unbindKeyHandlers(tile) {
+    // console.log("Attempting to remove keybinds...");
+    // debugger;
+    document.removeEventListener('keypress', document.handleKeypress);
+    document.removeEventListener('keydown', document.handleKeydown);
+}
 
 GameView.prototype.start = function start() {
     this.bindKeyHandlers(this.tile);
@@ -61,8 +75,10 @@ GameView.prototype.animate = async function animate(time) {
     const timeDelta = time - this.lastTime;
     this.game.step(timeDelta);
     if ( this.game.checkVerticalCollisions() ) {
-        newTile = this.game.addTile();
-        this.bindKeyHandlers(newTile);
+        // debugger;
+        this.unbindKeyHandlers(this.tile);
+        this.tile = this.game.addTile();
+        this.bindKeyHandlers(this.tile);
     }
     this.ctx.clearRect(0, 0, 600, 600);
     this.game.draw(this.ctx);
@@ -70,7 +86,8 @@ GameView.prototype.animate = async function animate(time) {
     const sleep = function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
-    await sleep(1000);
+    // await sleep(1000);
+    await sleep(400);
     requestAnimationFrame(this.animate.bind(this));
 };
 

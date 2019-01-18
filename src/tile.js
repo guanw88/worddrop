0b333ae2abc92048fbda4a7b1b24313040d65c51
gwang <guanw88@gmail.com>
2019-01-18 10:19:22 -0800

    Add tile scores to rendered tiles

--- src/tile.js
+++ src/tile.js
@@ -10,6 +10,7 @@ class Tile {
         this.letters = letters;
         this.currentLetterIdx = 0;
         this.letter = this.letters[this.currentLetterIdx];
+        this.score = this.calculateScore();
         this.x = x;
         this.y = y;
         this.movable = movable;
@@ -24,6 +25,11 @@ class Tile {
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
         ctx.fillText(this.letter, this.x + 30, this.y+32);
+        ctx.font = "16px Lato";
+        ctx.fillStyle = "black";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(this.score, this.x + 50, this.y + 48);
     }
 
     move(delta) {
@@ -44,6 +50,16 @@ class Tile {
     toggleLetter() {
         this.currentLetterIdx = (this.currentLetterIdx + 1) % this.letters.length;
         this.letter = this.letters[this.currentLetterIdx];
+        this.score = this.calculateScore();
+    }
+
+    calculateScore() {
+        const letter_scores = {
+            "A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4, "I": 1, "J": 8,
+            "K": 5, "L": 1, "M": 3, "N": 1, "O": 1, "P": 3, "Q": 10, "R": 1, "S": 1, "T": 1,
+            "U": 1, "V": 4, "W": 4, "X": 8, "Y": 4, "Z": 10
+        }; 
+        return letter_scores[this.letter];
     }
 
     isCollidedWithVertically(otherObject) {

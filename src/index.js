3012d9feffeb68e5190064a9b75374204890e360
gwang <guanw88@gmail.com>
2019-01-22 09:59:28 -0800

    Remove alert on game start

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
     const ctx = canvas.getContext("2d");
     ctx.fillStyle = "#ffffff";
     ctx.fillRect(0, 0, 150, 75);
-
-    alert("Welcome to WordDrop! Make words from the dropped tiles, then press SPACE to clear them! Press UP ARROW to toggle letters, LEFT/RIGHT to move, and DOWN to drop.")
     const game = new Game(ctx);
     new GameView(game, ctx).start();
 });
